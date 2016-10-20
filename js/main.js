f086d57aa865988de95210b78107abd4e76a68af
krzysko <krzyskowww@gmail.com>
2016-10-20 09:53:50 +0200

    jquery accordion done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+$(document).ready(function() {
+  $('.details').hide();
+  $('.projects h4').click(function() {
+    console.log('prjs li cklik');
+    var $details = $(this).next('.details');
+    if ($details.is(':hidden')) {
+      $('.details').hide();
+      $details.slideDown();
+      //$(this).addClass('close');
+    } else {
+      $details.fadeOut();
+      //$(this).removeClass('close');
+    }
+  }); // Koniec funkcji click.
+}); // Koniec funkcji ready.
